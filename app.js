c40fee6c9ee20da7ae6df0dfbd3361e968bd2357
Jeff Lewis <474547+halldorr@users.noreply.github.com>
2020-05-12 18:20:38 -0400

    Add files with keys

---
+++ app.js
@@ -0,0 +1,45 @@
+// Require discord.js package
+const Discord = require('discord.js');
+const axios = require('axios');
+
+// Create a new client using the new keyword
+const client = new Discord.Client();
+const api = '<openweathermap_api_key>';
+
+//secure token
+const {
+    token
+} = require("./token.json");
+
+// Display a message once the bot has started
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+//reconnect event
+client.on("reconnecting", () => {
+    console.log(`This bot is trying to reconnect: ${client.user.tag}!`);
+});
+
+// Check messages for a specific command
+client.on("message", async msg => {
+    const msgContent = msg.content.toLowerCase();
+    var command = msgContent.substr(0, msgContent.indexOf(' '));
+    var location = msgContent.substr(msgContent.indexOf(' ')+1);
+
+    // Send back a reply when the specific command has been written
+    if (command === "!weather") {
+        var dest = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&q=' + location + '&appid=' + api;
+        axios.get(dest)
+            .then(response => {
+                console.log(response.data.weather[0].description);
+                msg.reply(`It is currently ${response.data.main.temp} in ${location} with ${response.data.weather[0].description}`);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+});
+
+// Log in the bot with the token
+client.login(token);
